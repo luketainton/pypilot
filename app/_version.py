2cb2bc21906c0845dfd48a363ff5657e2d6c3390
Luke Tainton <luke@tainton.uk>
2022-06-25 21:45:16 +0100

    Create setup.py

---
+++ app/_version.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+
+"""MODULE: Specifies app version."""
+
+VERSION = "0.1"
---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+from app._version import VERSION
+
+setup(
+    name="ipilot",
+    version=VERSION,
+    description="IP Information Lookup Tool",
+    author="Luke Tainton",
+    author_email="luke@tainton.uk",
+    packages=["ipilot"],
+    install_requires=["requests"],
+    entry_points={
+        "console_scripts": [
+            "ipilot = app.main:main",
+        ],
+    },
+)
